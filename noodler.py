8e5d12a45ca931fc1b3eea6c9b7eb923dd80090f
Andrew Thorwall <andthorwall@gmail.com>
2023-02-21 19:15:49 -0500

    MoreUI

--- noodler.py
+++ noodler.py
@@ -1,15 +1,22 @@
-from PyQt6.QtWidgets import QApplication,QLabel,QSlider,QVBoxLayout,QWidget,QGraphicsScene,QGraphicsItem,QGraphicsView,QGraphicsSceneMouseEvent,QBoxLayout,QFrame
-from PyQt6.QtCore import Qt, QSize, QEvent, QTimer
-from PyQt6 import QtCore
+from PyQt6.QtWidgets import (
+    QApplication,
+    QFileDialog,
+    QFrame,
+    QGraphicsScene,
+    QGraphicsSceneMouseEvent,
+    QGraphicsView,
+    QInputDialog,
+    QMainWindow,
+)
+from PyQt6.QtCore import (
+    Qt, 
+    QTimer,
+)
 from PyQt6 import QtGui
 import transcribe
 from pytube import YouTube
-import json
 import librosa
-import numpy
 import os
-import pyaudio
-import time
 import math
 
 MUSIC_PATH = "music"
@@ -108,48 +115,6 @@ class AudioWaveformScene(QGraphicsScene):
             self.addRect(chunk_pixel_start, max_line_start, chunk_pixel_width, max_line_height, Qt.GlobalColor.blue, Qt.GlobalColor.blue)
             self.addRect(chunk_pixel_start, rms_line_start, chunk_pixel_width, rms_line_height, Qt.GlobalColor.darkBlue, Qt.GlobalColor.darkBlue)
 
-    # line-based waveform
-    # def add_waveform_to_scene(self, width, height, audio_data):
-    #     vertical_margin = height / 10
-    #     effective_height = height - vertical_margin * 2
-    #     subsamples = int(width * 2)
-    #     chunks = subsamples - 1
-    #     chunk_pixel_width = width / chunks
-    #     chunk_sample_width = len(audio_data) / chunks
-    #     last_x = None
-    #     last_y1 = None
-    #     last_y2 = None
-    #     last_rms_y1 = None
-    #     last_rms_y2 = None
-    #     for subsample in range(0, subsamples):
-    #         chunk_start = int(subsample * chunk_sample_width - chunk_sample_width * 0.5)
-    #         chunk_end = int(subsample * chunk_sample_width + chunk_sample_width * 0.5)
-    #         if chunk_start < 0:
-    #             chunk_start = 0
-    #         if chunk_end > len(audio_data):
-    #             chunk_end = len(audio_data)
-    #         chunk = audio_data[chunk_start:chunk_end]
-    #         (max, rms) = self.max_and_rms(chunk)
-    #         max_line_height = int(effective_height * max / 1.0)
-    #         max_line_start = int(vertical_margin + (effective_height - max_line_height) / 2)
-    #         rms_line_height = int(effective_height * rms / 1.0)
-    #         rms_line_start = int(vertical_margin + (effective_height - rms_line_height) / 2)
-    #         x = subsample * chunk_pixel_width
-    #         y1 = max_line_start
-    #         y2 = max_line_start + max_line_height
-    #         rms_y1 = rms_line_start
-    #         rms_y2 = rms_line_start + rms_line_height
-    #         if last_x != None:
-    #             self.view.scene().addLine(last_x, last_y1, x, y1, Qt.GlobalColor.red)
-    #             self.view.scene().addLine(last_x, last_y2, x, y2, Qt.GlobalColor.red)
-    #             self.view.scene().addLine(last_x, last_rms_y1, x, rms_y1, Qt.GlobalColor.red)
-    #             self.view.scene().addLine(last_x, last_rms_y2, x, rms_y2, Qt.GlobalColor.red)
-    #         last_x = x
-    #         last_y1 = y1
-    #         last_y2 = y2
-    #         last_rms_y1 = rms_y1
-    #         last_rms_y2 = rms_y2
-
     def max_and_rms(self, samples):
         total = 0
         max = 0
@@ -174,56 +139,47 @@ class AudioWaveformView(QGraphicsView):
         self.setFrameShape(QFrame.Shape.NoFrame)
         self.show()
 
-class Window(QWidget):
+class MainWindow(QMainWindow):
 
-    def __init__(self, transcriber):
+    def __init__(self):
         super().__init__()
-        self.transcriber = transcriber 
 
         self.resize(800, 600)
         self.setWindowTitle("Noodler")
 
-        self.audio_waveform = AudioWaveformView(transcriber, self)
-        self.audio_waveform.move(100, 100)
+        fileMenu = self.menuBar().addMenu("File")
+        openAction = fileMenu.addAction("Open...")
+        openAction.triggered.connect(self.open)
+        importAction = fileMenu.addAction("Import From YouTube...")
+        importAction.triggered.connect(self.import_from_youtube)
 
-        layout = QBoxLayout(QBoxLayout.Direction.TopToBottom)
-        layout.addWidget(self.audio_waveform)
-        self.setLayout(layout)
+    def open(self):
+        (path, _) = QFileDialog.getOpenFileName(None, "Open Audio File", "music", "Audio Files (*.mp4)");
+        self.load(path)
+
+    def import_from_youtube(self):
+        (url, _) = QInputDialog.getText(None, "Import From YouTube", "YouTube URL:")
+        if not os.path.exists("music"):
+            os.makedirs("music")
+        path = YouTube(url).streams.filter(only_audio = True).first().download(output_path="music")
+        self.load(path)
+
+    def load(self, path):
+        data, sampling_rate = librosa.load(path, sr=None, mono=False)
+        self.audio = transcribe.TranscribeContext(os.path.basename(path), data, sampling_rate, lambda: 0)
+        self.audio_waveform = AudioWaveformView(self.audio, self)
+        self.audio_waveform.move(100, 100)
+        self.setCentralWidget(self.audio_waveform)
 
     def keyPressEvent(self, a0: QtGui.QKeyEvent) -> None:
-        if transcriber.playing:
-            transcriber.stop()
+        if self.audio.playing:
+            self.audio.stop()
         else:
-            transcriber.play()
+            self.audio.play()
         return super().keyPressEvent(a0)
-
-def get_available_music():
-    return [f for f in os.listdir(MUSIC_PATH) if os.path.isfile(os.path.join(MUSIC_PATH, f))]
  
-print("\nWelcome to noodler.\n")
-if not os.path.exists("music"):
-    os.makedirs("music")
-available_music = get_available_music()
-print("Tracks available locally:\n")
-for (i, file) in enumerate(available_music):
-    print("{}) {}".format(i + 1, os.path.basename(file)))
-print("")
-track = input("Enter the URL of a YouTube video to transcribe, or select a song from above: ")
-path = None
-if track.isnumeric():
-    index = int(track) - 1
-    if index < 0 or index >= len(available_music):
-        print("Invalid selection")
-        exit()
-    path = os.path.join(MUSIC_PATH, available_music[index])
-else:
-    path = YouTube(track).streams.filter(only_audio = True).first().download(output_path="music")
-print("Downloaded music to " + path)
-data, sampling_rate = librosa.load(path, sr=None, mono=False)
-transcriber = transcribe.TranscribeContext(os.path.basename(path), data, sampling_rate, lambda: 0)
-
 app = QApplication([])
 app.setStyle('macos')
-window = Window(transcriber)
+window = MainWindow()
 window.show()
 app.exec()
